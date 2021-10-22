8c8a1eea5f36ba4f6b699e2f59e2e98738c647c2
Rogerio Cruz <rogvc@outlook.com>
2021-10-21 22:48:17 -0600

    Completes the Tutorial

--- apple-swift-ui-tutorials/creating-and-combining-views/Landmarks/Landmarks/ContentView.swift
+++ apple-swift-ui-tutorials/creating-and-combining-views/Landmarks/Landmarks/ContentView.swift
@@ -3,18 +3,41 @@ import CoreData
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    
+                    Spacer()
+                    
+                    Text("California")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About")
+                    .font(.title2)
+                Text("Desription goes here.")
+                
             }
+            .padding()
+            Spacer()
         }
-        .padding()
     }
 }
 struct ContentView_Previews: PreviewProvider {
