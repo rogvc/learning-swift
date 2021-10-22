3174ba5dd02ee808e8683adfff71fe2793dfa68c
Rogerio Cruz <rogvc@outlook.com>
2021-10-21 22:21:36 -0600

    Completes Section 3

--- apple-swift-ui-tutorials/creating-and-combining-views/Landmarks/Landmarks/ContentView.swift
+++ apple-swift-ui-tutorials/creating-and-combining-views/Landmarks/Landmarks/ContentView.swift
@@ -3,9 +3,18 @@ import CoreData
 
 struct ContentView: View {
     var body: some View {
-        Text("Turtle Rock")
-            .font(.title)
-            .padding()
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
     }
 }
 struct ContentView_Previews: PreviewProvider {
